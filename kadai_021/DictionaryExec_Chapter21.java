badb28661c0e1964ee0104b2b023496c46349c53
Tacty0210 <saaya66@gmail.com>
2024-08-29 21:53:44 +0900

    kadai_21を再提出

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -1,16 +1,23 @@
 package kadai_021;
 
-public class DictionaryExec_Chapter21 extends Dictionary_Chapter21 {
+import java.util.ArrayList;
+
+public class DictionaryExec_Chapter21 {
 
 	public static void main(String[] args) {
-		System.out.println("appleの意味は" + dictionary.get("apple"));
-		System.out.println("bananaの意味は" + dictionary.get("banana"));
-		System.out.println("grapeの意味は" + dictionary.get("grape"));
 		
-		String Error = dictionary.get("orange");
-		if (Error == null) {
-			System.out.println("orangeは辞書に存在しません");
-		}
-	}
+		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
+        dictionary.naiyo();
 
+        // 検索する単語の一覧
+        ArrayList<String> fruitList = new ArrayList<String>();
+        fruitList.add("apple");
+        fruitList.add("banana");
+        fruitList.add("grape");
+        fruitList.add("orage");
+        
+
+        // 繰り返し処理で検索
+        dictionary.serchMultiple(fruitList);
+	}
 }
--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -1,14 +1,15 @@
 package kadai_021;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class Dictionary_Chapter21 {
+
 	
-	// 英単語の辞書として機能するクラス変数
-		protected static HashMap<String, String> dictionary = new HashMap<>();
-		
-		static {
-			// dictionaryの初期化
+		// 英単語の辞書として機能するクラス変数
+		HashMap<String, String> dictionary = new HashMap<String, String>();
+			
+			public void naiyo() {
 			dictionary.put("apple", "りんご");
 			dictionary.put("peach", "桃");
 			dictionary.put("banana", "バナナ");
@@ -19,9 +20,20 @@ public class Dictionary_Chapter21 {
 			dictionary.put("grape", "ぶどう");
 			dictionary.put("muscat", "マスカット");
 			dictionary.put("cherry", "さくらんぼ");
-		}
-
-		public static void main(String[] args) {
-			// メインメソッドは特に何も行動しない場合は空でもよい
-		}
+			}
+					
+			public void serch(String word) {
+				if (dictionary.get(word) == null) {
+					System.out.println(word + "は辞書に存在しません");
+				}else {
+					System.out.println(word + "の意味は" + dictionary.get(word) + "です");
+				}
+			}
+			
+			public void serchMultiple(List<String> words) {
+		        for (String word : words) {
+		            serch(word);
+		        }
+		    }
+				
 }
